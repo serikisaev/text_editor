b7b0d0f81c5caf86ebfcf5dae3fef7d281c0bea1
serik <serikisaev87@gmail.com>
2020-06-01 18:36:47 +0500

    add code

--- text_edit/views.py
+++ text_edit/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 
 def index(request):
+    return render(request, 'index.html')
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
     return render(request, 'index.html')
